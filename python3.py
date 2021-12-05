a458fd9447c7aefca113103fe04c3ecd8193add2
pooja9983 <69471390+pooja9983@users.noreply.github.com>
2021-12-05 12:14:19 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,6 @@
+list1=[12,-7,5,64,-14]
+pos_num = [num for num in list1 if num >= 0]
+print("positive numbers in the list1",*pos_num)
+list2=[12,14,-95,3]
+pos_num = [num for num in list2 if num >= 0]
+print("positive numbers in the list2",*pos_num)
